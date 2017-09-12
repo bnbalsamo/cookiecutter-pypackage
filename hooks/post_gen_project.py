b2a2515b5e637484deafd054e3d1a04dcbfba3c0
brian <balsamo@uchicago.edu>
2017-09-12 13:53:59 -0500

    Creates a .env venv directory for a new project if possible

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,25 @@
-import re
-import sys
-import os
-from argparse import ArgumentParser
+from sys import version_info
 
-from venv import EnvBuilder
+print("Template successfully created.")
 
-EnvBuilder().create(os.path.join(os.getcwd(), '.venv'))
+if version_info < (3, 3):
+    # Can't build the venv, alert but don't fail.
+    print("Incompatible Python (<3.3). " +
+          "Virtual environment must be created manually")
+    exit(0)
+
+from os import getcwd
+from os.path import join
+from venv import main
+
+
+cookiecutter_args = "{{cookiecutter.venv_args}}"
+args = "{}".format(join(getcwd(), '.env'))
+if cookiecutter_args != "":
+    args = "{} {}".format(cookiecutter_args, args)
+main(args.split())
+print("Virtual environment successfully created. Activate " +
+      "it with: \n" +
+      "$ source .env/bin/activate \n" +
+      "from the project root")
+exit(0)
